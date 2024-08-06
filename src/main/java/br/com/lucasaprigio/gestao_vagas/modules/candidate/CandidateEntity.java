dc2e92acd98014ded39b479a33497a7fac52049c
User <Lucas Aprigio>
2024-08-06 08:30:59 -0300

    Candidate Table

--- src/main/java/br/com/lucasaprigio/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/lucasaprigio/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,26 @@
 package br.com.lucasaprigio.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data // Importação feita do Lombok para colocar os getters e setters, sem ter que sujar o código.
+@Entity(name = "candidate") // Definimos o nome da tabela.
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue()
     private UUID id;
+
     private String name;
 
     @Pattern(regexp = "\\S+", message= "O campo [username] não deve conter espaço")
@@ -24,4 +33,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
